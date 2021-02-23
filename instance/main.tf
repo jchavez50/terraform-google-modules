ef15f6dbc55738616d377c9d93fbcde612df2a80
joseph chavez <ajoey1718@gmail.com>
2021-02-22 22:39:09 -0800

    changes on conditional tests pass

--- instance/main.tf
+++ instance/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "default" {
     count = 1
     name = "count.index+1"
     # conditional (IF statements)
-    machine_type = var.environment == "production" ? var.machine_type : var.machine_type 
+    machine_type = var.environment == "production" ? var.machine_type : var.machine_type_dev 
     zone = "us-west1-a"
 
     boot_disk {
@@ -14,13 +14,7 @@ resource "google_compute_instance" "default" {
     network_interface {
       network = "default" 
     }
-
-
-
-service_account {
-  scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+    service_account {
+      scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+    }
 }
-   
- 
-
-
